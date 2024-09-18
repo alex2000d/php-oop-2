47c090bda9eacebb2c7df6a4d0e75b32f60557a8
alex2000d <demarcoalessandro777@gmail.com>
2024-09-18 17:23:08 +0200

    [UPD] created all my classes

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  // creo la mia classe articolo
+  // creo la mia classe articolo che sarà la mia classe padre 
 class Articolo {
     public $titolo;
     public $prezzo;
@@ -7,6 +7,40 @@ class Articolo {
     public function __construct($_titolo, $_prezzo) {
         $this->titolo = $_titolo;
         $this->prezzo = $_prezzo;
+    }
+}
+
+// creo la mia classe categoria 
+class Categoria {
+    public $nome;
+    public $icona;
+
+    public function __construct($nome, $icona) {
+        $this->nome = $nome;
+        $this->icona = $icona;
+    }
+}
+// creo la classe tipo di articolo
+class TipoArticolo {
+    public $nome;
+
+    public function __construct($nome) {
+        $this->nome = $nome;
+    }
+}
+
+// Creo la mia classe prodotto che sara figlia di articolo ed erediterà qualcosa dal padre
+class Prodotto extends Articolo {
+    public $immagine;
+    public $categoria;
+    public $tipoArticolo;
+
+    public function __construct($titolo, $prezzo, $immagine, $categoria, $tipoArticolo) {
+        // Richiamo le proprietà del genitore 
+        parent::__construct($titolo, $prezzo); 
+        $this->immagine = $immagine;
+        $this->categoria = $categoria;
+        $this->tipoArticolo = $tipoArticolo;
     }
 }
 ?>
