8b33035e58b997d4c7749946bb61defaf9d18e1e
alex2000d <demarcoalessandro777@gmail.com>
2024-09-18 18:21:58 +0200

    [UPD] print the card

--- index.php
+++ index.php
@@ -45,8 +45,8 @@ class Prodotto extends Articolo {
 }
 
 // Creo le mie categorie
-$categoriaCani = new Categoria("Cani", 'https://fontawesome.com/icons/dog?f=classic&s=solid');
-$categoriaGatti = new Categoria("Gatti", "https://fontawesome.com/icons/cat?f=classic&s=solid");
+$categoriaCani = new Categoria("Cagnolini","cani");
+$categoriaGatti = new Categoria("Gattini","gatti");
 
 // Creo i tipi di articoli
 $tipoCibo = new TipoArticolo("Cibo");
@@ -58,9 +58,14 @@ $prodotto1 = new Prodotto("Crocchette per Cani", 25.99, "crocchette_cani.jpg", $
 $prodotto2 = new Prodotto("Palla per Gatti", 10.50, "palla_gatti.jpg", $categoriaGatti, $tipoGioco);
 
 // visualizzo i miei prodotti a schermo
-var_dump($prodotto1);
-var_dump($prodotto2);
+// var_dump($prodotto1);
+// var_dump($prodotto2);
 
+// creo il mio arrey con i prodotti da visualizzare
+$prodotti = [
+    $prodotto1,
+    $prodotto2
+];
 
 ?>
 
@@ -69,10 +74,32 @@ var_dump($prodotto2);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>E-commerce</title>
 </head>
 <body>
-    
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-center">Cats & Dogs world</h1>
+            <p class="text-center">qui puoi trovare tutto ciò che hai bisogno per i tuoi amici pelosetti</p>
+        </div>
+    </div>
+    <div class="row mt-5">
+        <?php foreach ($prodotti as $prodotto): ?>
+            <div class="col-12 col-lg-4">
+                <div class="card">
+                    <img src="img/<?= $prodotto->immagine; ?>" class="card-img-top" alt="<?= $prodotto->titolo; ?>">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $prodotto->titolo; ?></h5>
+                        <p class="card-text">Prezzo: €<?= $prodotto->prezzo; ?></p>
+                        <p class="card-text">Categoria: <?= $prodotto->categoria->icona; ?> <?= $prodotto->categoria->nome; ?></p>
+                        <p class="card-text">Tipo di articolo: <?= $prodotto->tipoArticolo->nome; ?></p>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
 </body>
 </html>
